3e030c48fe4ab3f5d290cb11b66222960471a5a8
yinhla <makamuyinhla2016@gmail.com>
2025-04-22 21:00:39 +0200

    created routing

---
+++ src/pages/index.jsx
@@ -0,0 +1,21 @@
+import '../css/index.css'
+
+
+
+function homepage(){
+
+    const name = "yinhla";
+
+
+
+    return (
+        <div className="d-flex">
+            <div className="fs-1">asdasd</div>
+            <h1 >{name}</h1>
+        </div>
+    );
+}
+
+
+export default homepage;
+
